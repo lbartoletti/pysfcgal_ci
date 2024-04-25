c7c9e1d638ba52513ff0635365b98928974fb5f7
Jean Felder <jean.felder@oslandia.com>
2024-04-25 16:06:31 +0200

    ci: Build windows wheel with OSGeo4W
    This introduces an initial CI with a `build` stage. This stage
    contains a job to build a windows wheel based on the python version
    from OSGeo4W. It is based on a docker image from SFCGAL registry which
    contains a compiled SFCGAL and its dependencies installed with vcpkg.
    
    This job has the following rules:
     - optional when a new merge request is created
     - automatic on a push to the master branch or when a new tag is
     created.

---
+++ ci/build.ps1
@@ -0,0 +1,14 @@
+# Install python3 from OSGeo4W
+Invoke-WebRequest -Uri "https://download.osgeo.org/osgeo4w/osgeo4w-setup-v2.exe" -OutFile "osgeo4w-setup-v2.exe"
+Start-Process -FilePath "osgeo4w-setup-v2.exe" -ArgumentList "--advanced --autoaccept --delete-orphans --no-desktop --quiet-mode --upgrade-also --site https://download.osgeo.org/osgeo4w/v2 --packages python3-core,python3-pip,python3-devel" -Wait
+Remove-Item -Path .\osgeo4w-setup-v2.exe -Force
+C:\OSGeo4W\OSGeo4W.bat python3 -m pip install -U pip setuptools wheel delvewheel
+
+# Generate the wheel
+$env:INCLUDE='c:\SFCGAL\build\include\'
+$env:LIB='c:\SFCGAL\build\src\Release\'
+C:\OSGeo4W\OSGeo4W.bat python3 setup.py build sdist bdist_wheel
+
+# Add sfcgal dll with delvewheel
+$env:WHEEL_NAME=Get-ChildItem -Path dist/* -Include *.whl
+C:\OSGeo4W\OSGeo4W.bat delvewheel repair --add-path $env:LIB -w dist $env:WHEEL_NAME
