59a156d37ea2e4c5af06263f695d362d8cbef655
Jean Felder <jean.felder@oslandia.com>
2024-08-20 11:11:02 +0200

    ci: Do not directly invoke setup.py to build windows package
    This method is now deprecated and generates the following warning:
    ```
    C:\OSGeo4W\apps\Python312\Lib\site-packages\setuptools\_distutils\cmd.py:66: SetuptoolsDeprecationWarning: setup.py install is deprecated.
    !!
            ********************************************************************************
            Please avoid running ``setup.py`` directly.
            Instead, use pypa/build, pypa/installer or other
            standards-based tools.
            See https://blog.ganssle.io/articles/2021/10/setup-py-deprecated.html for details.
            ********************************************************************************
    !!
    ```
    
    This was not possible to avoid this logic because the C files were not
    part of the `package_data`. This is now fixed.
    `python3 setup.py bdist_wheel` is now replaced by `python3 -m build`
    
    Related: https://gitlab.com/sfcgal/pysfcgal/-/merge_requests/42

--- ci/build.ps1
+++ ci/build.ps1
@@ -2,12 +2,12 @@
 Invoke-WebRequest -Uri "https://download.osgeo.org/osgeo4w/osgeo4w-setup-v2.exe" -OutFile "osgeo4w-setup-v2.exe"
 Start-Process -FilePath "osgeo4w-setup-v2.exe" -ArgumentList "--advanced --autoaccept --delete-orphans --no-desktop --quiet-mode --upgrade-also --site https://download.osgeo.org/osgeo4w/v2 --packages python3-core,python3-pip,python3-devel" -Wait
 Remove-Item -Path .\osgeo4w-setup-v2.exe -Force
-C:\OSGeo4W\OSGeo4W.bat python3 -m pip install -U pip setuptools wheel delvewheel
+C:\OSGeo4W\OSGeo4W.bat python3 -m pip install -U pip build delvewheel
 
 # Generate the wheel
 $env:INCLUDE='c:\SFCGAL\build\include\'
 $env:LIB='c:\SFCGAL\build\src\Release\'
-C:\OSGeo4W\OSGeo4W.bat python3 setup.py build sdist bdist_wheel
+C:\OSGeo4W\OSGeo4W.bat python3 -m build
 
 # Add sfcgal dll with delvewheel
 $env:WHEEL_NAME=Get-ChildItem -Path dist/* -Include *.whl
