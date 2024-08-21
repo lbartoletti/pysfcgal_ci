lbartoletti/pysfcgal_ci

commit b1340def59a7cc5816e913ab39c57c099af21ca1

sdist: add C files to package_data

setup.py
@@ -17,8 +17,9 @@ setup(
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
     ],
     packages=["pysfcgal"],
+    package_data={"pysfcgal": ["pysfcgal/*.c"]},
     setup_requires=["cffi>=1.0.0"],
     cffi_modules=["pysfcgal/sfcgal_build.py:ffibuilder"],
     install_requires=["cffi>=1.0.0"],
-    extras_require = {'contract': ["icontract>=2.6.0"]}
+    extras_require={"contract": ["icontract>=2.6.0"]}
 )

commit 1e03452323a66071cd0b30f94be57b093cf8e0c9

setup: Fix package_data definition
The dict key already defines the package name "pysfcgal". It means
that the installation process looks for the c file in the
`pysfcgal/pysfcgal` directory.

This issue is fixed by removing the extra `pysfcgal`.

Fixes: b1340def59a7cc5816e913ab39c57c099af21ca1

setup.py
@@ -17,7 +17,7 @@ setup(
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
     ],
     packages=["pysfcgal"],
-    package_data={"pysfcgal": ["pysfcgal/*.c"]},
+    package_data={"pysfcgal": ["*.c"]},
     setup_requires=["cffi>=1.0.0"],
     cffi_modules=["pysfcgal/sfcgal_build.py:ffibuilder"],
     install_requires=["cffi>=1.0.0"],