9312fdc7cd8e59cbc3b7e6739d6d1d07a375dab6
Raphaël Delhome <raphael.delhome@oslandia.com>
2024-09-19 10:50:37 +0200

    feat: update sfcgal_def_msvc
    The update_def.sh script is updated so as to update
    `pysfcgal_def_msvc.c` too.
    
    One uses a `ed` command to remove the Alpha shape entries from this
    alternative C API (it does work for the MSVC compiler).

--- update_def.sh
+++ update_def.sh
@@ -5,6 +5,16 @@ if [ "$#" -ne 1 ] || ! [ -f "$1" ]; then
   exit 1
 fi
 
+# sfcgal_def.c
 sed -e '4,/endif/d;/__cplusplus/,$d' -e "s/SFCGAL_API //" -e "/^#if/d" -e "/^#endif/d" ${1} > pysfcgal/sfcgal_def.c
 printf "void\nfree(void*);" >> pysfcgal/sfcgal_def.c
+echo "pysfcgal/sfcgal_def.c has been updated!"
 
+# sfcgal_def_msvc.c
+sed -e '4,/endif/d;/__cplusplus/,$d' -e "s/SFCGAL_API //" -e "/^#if/d" -e "/^#endif/d" ${1} > pysfcgal/sfcgal_def_msvc.c
+# remove the Alpha shapes entries from the C API for the MSVC compiler
+# found in https://stackoverflow.com/questions/876446/how-do-i-delete-a-matching-line-the-line-above-and-the-one-below-it-using-sed
+ed -s pysfcgal/sfcgal_def_msvc.c <<< '/^sfcgal_geometry_alpha_shapes.*/ -1, /^sfcgal_geometry_alpha_shapes.*/ +1 d'$'\n'w
+ed -s pysfcgal/sfcgal_def_msvc.c <<< '/^sfcgal_geometry_optimal_alpha_shapes.*/ -1, /^sfcgal_geometry_optimal_alpha_shapes.*/ +1 d'$'\n'w
+printf "void\nfree(void*);" >> pysfcgal/sfcgal_def_msvc.c
+echo "pysfcgal/sfcgal_def_msvc.c has been updated!"
